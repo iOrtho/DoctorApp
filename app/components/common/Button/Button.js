b193d2305946f12de1019e3e973aa66f1a3c3eba
FrenchMajesty <verdi.kap@gmail.com>
2018-06-26 07:27:30 +0200

    Create Button component
    As a wrapper on the native-base Button to avoid repeating myself and simplify the API.

---
+++ app/components/common/Button/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, ActivityIndicator as Spinner } from 'react-native';
+import { Button as NativeButton } from 'native-base';
+import style from './style';
+
+const Button = ({loading, text, children, style: customStyle, ...others}) => {
+	
+	return (
+		<NativeButton {...others} style={[style.button, ...customStyle]}>
+			{(!loading && text) && <Text style={[style.text]}>{text}</Text>}
+			{(!loading && children) && <div>{children}</div>}
+			{loading &&  <Spinner style={[style.spinner]} color="#fff" />}
+		</NativeButton>
+	);
+}
+
+Button.propTypes = {
+	text: PropTypes.string,
+	loading: PropTypes.bool,
+	style: PropTypes.array,
+	children: PropTypes.node,
+};
+
+Button.defaultProps = {
+	loading: false,
+	text: null,
+	style: [],
+};
+
+export default Button;
---
+++ app/components/common/Button/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	button: {
+		padding: 5,
+		marginTop: 15,
+		marginBottom: 15,
+		width: '100%',
+		display: 'flex',
+	},
+	text: {
+		flex: 1,
+		color: '#fff',
+		textAlign: 'center',
+	},
+	spinner: {
+		flex: 1,
+	}
+});
