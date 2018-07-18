65c07344520ba71474cab187fa56ff2f9bdd5ba7
FrenchMajesty <verdi.kap@gmail.com>
2018-07-18 23:35:32 +0200

    #9: Modify QuickHelp component to add support for modals

--- app/components/Home/QuickHelp/QuickHelp.js
+++ app/components/Home/QuickHelp/QuickHelp.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button } from 'native-base';
+import { Button, ActionSheet } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AppButton from 'app/components/common/Button';
 import styling from 'app/config/styling';
 import style from './style';
 
@@ -14,6 +15,8 @@ class QuickHelp extends Component {
 		super(props);
 
 		this.state = this.getInitialState();
+
+		this.renderMapModal = this.renderMapModal.bind(this);
 	}
 
 	/**
@@ -26,8 +29,18 @@ class QuickHelp extends Component {
 				{icon: 'phone', title: 'Call', onPress: null},
 				{icon: 'map-marker', title: 'Directions', onPress: null, iconSize: 26},
 				{icon: 'share', title: 'Share', onPress: null},
-			]
+			],
+			mapModal: true,
 		};
+	}
+
+	openMapMenu() {
+		const {} = this.props.office;
+
+		ActionSheet.show({
+			options: ['Open in Maps', 'Copy Address', 'Cancel'],
+			cancelButtonIndex: 2,
+		})
 	}
 
 	/**
@@ -39,17 +52,20 @@ class QuickHelp extends Component {
 		const {style: customStyle} = this.props;
 
 		return (
-			<View style={[style.container, ...customStyle]}>
-				{buttons.map(({icon, title, onPress, iconSize}, i) => {
-					return (
-						<View key={i} style={[style.buttonWrapper]}>
-							<Button style={[style.button]} onPress={onPress} bordered transparent>
-								<Icon name={icon} color={styling.color.main} size={iconSize || 23} />
-							</Button>
-							<Text style={[style.buttonTitle]}>{title.toUpperCase()}</Text>
-						</View>
-					);
-				})}
+			<View>
+				<View style={[style.container, ...customStyle]}>
+					{buttons.map(({icon, title, onPress, iconSize}, i) => {
+						return (
+							<View key={i} style={[style.buttonWrapper]}>
+								<Button style={[style.button]} onPress={onPress} bordered transparent>
+									<Icon name={icon} color={styling.color.main} size={iconSize || 23} />
+								</Button>
+								<Text style={[style.buttonTitle]}>{title.toUpperCase()}</Text>
+							</View>
+						);
+					})}
+				</View>
+				{this.renderMapModal()}
 			</View>
 		);
 	}
