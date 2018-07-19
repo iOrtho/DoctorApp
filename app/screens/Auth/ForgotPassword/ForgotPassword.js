2c61c90accaeef4308e4d6961d2b26725e41cd38
FrenchMajesty <verdi.kap@gmail.com>
2018-07-19 23:07:30 +0200

    #8 : Create ForgotPassword screen

---
+++ app/screens/Auth/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import { View, Text, Spinner } from 'react-native';
+import { Form, Input, Item, Label } from 'native-base';
+import { connect } from 'react-redux';
+import UserAction from 'app/store/actions/user';
+import { auth, database } from 'app/config/firebase';
+import ScreenWrapper from 'app/components/common/ScreenWrapper/';
+import PhoneNumberVerification from 'app/components/PhoneNumberVerification/';
+import Button from 'app/components/common/Button/';
+import style from './style';
+
+class ForgotPassword extends Component {
+
+	/** The component's constructor */
+	constructor(props) {
+		super(props);
+
+		this.state = this.getInitialState();
+
+		this.handleResetPassword = this.handleResetPassword.bind(this);
+	}
+
+	/**
+	 * Return the component's initial state
+	 * @return {Object} 
+	 */
+	getInitialState() {
+		return {
+			stage: 1,
+			password: '',
+			repeated: '',
+			isSubmitting: false,
+		};
+	}
+
+	/**
+	 * Perform validation on the password before updating it
+	 * @return {Void} 
+	 */
+	handleResetPassword() {
+		const {password, repeated} = this.state;
+		const {navigation} = this.props;
+
+		if(password.length < 6) {
+			alert('Your password must be at least 6 characters.');
+			return;
+		}
+
+		if(password != repeated) {
+			alert('The passwords entered do not match.');
+			return;
+		}
+
+		this.setState({isSubmitting: true});
+		auth().currentUser.updatePassword(password)
+			.then(() => navigation.reset('Login'))
+			.catch(console.error);
+	}
+
+	/**
+	 * Render the component's markup
+	 * @return {ReactElement} 
+	 */
+	render() {
+		const {stage, password, repeated, isSubmitting} = this.props;
+		const {office} = this.props;
+		
+		return (
+			<ScreenWrapper>
+				<Text style={[style.title]}>Verify your number</Text>
+					{stage == 1 && 
+						<PhoneNumberVerification
+							purpose="reset"
+							officeId={office.id}
+							onSuccess={() => this.setState({stage: 2})}
+							style={[{marginTop: 30}]}
+						/>}
+
+					{stage == 2 &&
+						<Form>
+							<Text>Make sure that your password is at least 6 characters.</Text>
+							<Item floatingLabel>
+								<Label>New password</Label>
+								<Input
+									autoCapitalize="none"
+									autoCorrect={false}
+									secureTextEntry={true}
+									value={password}
+									onChangeText={(password) => this.setState({password})}
+								/>
+							</Item>
+
+							<Item floatingLabel>
+								<Label>New password repeated</Label>
+								<Input
+									autoCapitalize="none"
+									autoCorrect={false}
+									secureTextEntry={true}
+									value={repeated}
+									onChangeText={(repeated) => this.setState({repeated})}
+								/>
+							</Item>
+							<Button
+								text="Reset password"
+								loading={isSubmitting}
+								onPress={this.handleResetPassword}
+							/>
+						</Form>}
+			</ScreenWrapper>
+		);
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ForgotPassword);
+
+/**
+ * Map the redux store's state to the component's props
+ * @param  {Object} options.office The office model
+ * @return {Object}                  
+ */
+function mapStateToProps({office}) {
+	return {
+		office,
+	};
+}
+
+/**
+ * Map the store's action dispatcher to the component's props
+ * @param  {Function} dispatch The dispatch function
+ * @return {Object}           
+ */
+function mapDispatchToProps(dispatch) {
+	return {
+		setUserModel: (data) => dispatch(UserAction.setUserModel(data)),
+	};
+}
