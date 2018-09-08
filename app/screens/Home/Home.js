51068cb738f4fffdd9a5b1d8a1d30522d2f91eb2
FrenchMajesty <verdi.kap@gmail.com>
2018-09-08 01:16:19 -0500

    #34 : Add office and doctor data to `Home` screen

--- app/screens/Home/Home.js
+++ app/screens/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 import ScreenWrapper from 'app/components/common/ScreenWrapper';
 import ScreenLoading from 'app/components/common/ScreenLoading';
 import QuickHelp from 'app/components/Home/QuickHelp';
 import OperatingHours from 'app/components/Home/OperatingHours';
 import Slide from 'app/components/Slide';
+import Permissions from 'app/lib/Permissions';
 import { connect } from 'react-redux';
 import UserAction from 'app/store/actions/user';
 import style from './style';
@@ -28,12 +29,25 @@ class Home extends Component {
 	getInitialState() {
 		return {
 			data: [
-				{ url: 'https://i.imgur.com/MtjAPCJ.png' },
+				/*{ url: 'https://i.imgur.com/MtjAPCJ.png' },
 				{ url: 'https://i.imgur.com/VHE366s.png' },
-				{ url: 'https://i.imgur.com/jHyN5vz.png' },
+				{ url: 'https://i.imgur.com/jHyN5vz.png' },*/
 			],
 			loadQueue: []
 		};
+	}
+
+	/**
+	 * Request to activate notifications if not already done and load all the pics
+	 */
+	componentDidMount() {
+		const {office: {doctors, pictures}} = this.props;
+		const data = pictures.map(url => ({url}));
+		this.setState({data});
+		
+		Image.prefetch(pictures[0]);
+		Image.prefetch(doctors[0].picture);
+		Permissions.requestNotifications(this.props.user.id);
 	}
 
 	/**
@@ -55,6 +69,11 @@ class Home extends Component {
 	render() {
 		const {office} = this.props;
 		const {data, loadQueue} = this.state;
+		const doctorImage = {
+			width: '100%',
+			height: 220,
+			marginBottom: 20,
+		}
 
 		if(!office.id) {
 			return <ScreenLoading />;
@@ -86,22 +105,26 @@ class Home extends Component {
 
 					{office.ready && <OperatingHours hours={office.operating_hours} />}
 
-					<View style={{height: 200, backgroundColor: 'lime'}}>
+					<View style={{height: 75, marginTop: 20, marginBottom: 20, backgroundColor: 'lime'}}>
 						<Text>Review container</Text>
 					</View>
 
-					<View style={[style.bioContainer]}>
-						<Text style={[style.bio]}>{office.description}</Text>
+					<Text style={[style.subtitle, {marginBottom: 10}]}>Behind our practice...</Text>
+
+					<View>
+						<Text style={[style.paragraph]}>{office.description}</Text>
 					</View>
 
-					<Text style={[style.title]}>About the Doctor</Text>
+					<Text style={[style.title, {marginBottom: 10}]}>About our Doctor</Text>
 
-					<View style={{height: 200, backgroundColor: 'turquoise'}}>
-						<Text>Picture of Doctor?</Text>
+					<Text style={[style.subtitle]}>{office.doctors[0].name}</Text>
+
+					<View style={doctorImage}>
+						<Image source={{uri: office.doctors[0].picture}} style={{width: '100%', height: '100%'}} />
 					</View>
 
-					<View style={[style.bioContainer]}>
-						<Text style={[style.bio]}>{office.doctors[0].biography}</Text>
+					<View style={{marginBottom: 25}}>
+						<Text style={[style.paragraph]}>{office.doctors[0].biography}</Text>
 					</View>
 				</ScreenWrapper>
 			</ScrollView>
@@ -114,11 +137,13 @@ export default connect(mapStateToProps, mapDispatchToProps)(Home);
 /**
  * Map the redux store's state to the component's props
  * @param  {Object} options.office The office model
+ * @param  {Object} options.user The user model
  * @return {Object}                  
  */
-function mapStateToProps({office}) {
+function mapStateToProps({office, user}) {
 	return {
 		office,
+		user,
 	};
 }
 
--- app/screens/Home/style.js
+++ app/screens/Home/style.js
@@ -8,10 +8,13 @@ export default StyleSheet.create({
 		marginTop: 20,
 		marginBottom: 20,
 	},
-	bioContainer: {
-		padding: 20,
-	},
-	bio: {
-		lineHeight: 24,
-	},
+    subtitle: {
+        fontSize: 16,
+        flex: 1,
+        fontWeight: '500',
+        marginBottom: 20,
+    },
+    paragraph: {
+        lineHeight: 25,
+    },
 });
